344a7813200847bf31e44b94a357297f3fd2efcd
Linda Kovacs <lindaprovenza@yahoo.com>
2017-01-20 20:06:32 -0500

    Delete database.php

--- Shoutit-Shoutbox/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Connect to MySQL
-$con = mysqli_connect("localhost","root","121212aa","shoutit");
-//Test Connection
-if(mysqli_connect_errno()){
-	echo 'Failed to connect to MySQL: '.mysqli_connect_error();
-}
